3e44c6124f099284cad325d133cf697b05a11558
jisha-mcabee <jisha@mcabee.in>
2025-02-23 16:26:44 +0530

    fix path

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,18 @@
 import localFont from "next/font/local";
 
 const aeonik_bold = localFont({
-  src: "./font/AeonikBold.otf",
+  src: "/font/AeonikBold.otf",
 });
 const aeonik_bold_italic = localFont({
-  src: "./font/AeonikBolditalic.otf",
+  src: "/font/AeonikBolditalic.otf",
 });
-const aeonik_light = localFont({ src: "./font/AeonikLight.otf" });
+const aeonik_light = localFont({ src: "/font/AeonikLight.otf" });
 const aeonik_light_italic = localFont({
-  src: "./font/AeonikLightitalic.otf",
+  src: "/font/AeonikLightitalic.otf",
 });
-const aeonik_regular = localFont({ src: "./font/AeonikRegular.otf" });
+const aeonik_regular = localFont({ src: "/font/AeonikRegular.otf" });
 const aeonik_regular_italic = localFont({
-  src: "./font/AeonikRegularitalic.otf",
+  src: "/font/AeonikRegularitalic.otf",
 });
 
 export default {
